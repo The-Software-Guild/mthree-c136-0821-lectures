f6a62bba42d15244d82ad9138e57d41514eb71be
Patrick <pjmorgan716@gmail.com>
2021-08-24 14:09:29 -0400

    instructions hot fix

--- lectures/node/day1/assignments/node-streams-fs-events.js
+++ lectures/node/day1/assignments/node-streams-fs-events.js
@@ -19,7 +19,14 @@
 
 // 3. express.js exercise
 
+// Create an application with following features:
 
+// Application will make a call to the API (http://5c055de56b84ee00137d25a0.mockapi.io/api/v1/employees) using Request Object.
+// Once the data is available, the employee list along with the details should be visible to the User on the client side front end wed page.
+// The employee details would contain:
+//     - Name
+//     - Id
+//     - CreatedAt
 
 
 
