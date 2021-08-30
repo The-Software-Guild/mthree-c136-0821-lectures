457c1edd26a7ce941b001a3ee17eef35742d054a
Patrick <pjmorgan716@gmail.com>
2021-08-30 12:35:10 -0400

    end

--- lectures/react/day0/main.js
+++ lectures/react/day0/main.js
@@ -1,7 +1,17 @@
 // main.js
 
 // console.log(document)
-// console.dir(document)
+// console.dir(document) // dir gives us an object like representation of a thing that is not an object
+
+
+// console.log(document.body)
+
+
+
+
+
+
+
 
 
 
--- lectures/react/day0/notes.js
+++ lectures/react/day0/notes.js
@@ -8,21 +8,53 @@ const someArray = [0, true, 4, 199]
 // and then we have a new itteration through the new array to do another thing
 
 
+// DOM
 
+
+// JS provides us a document object in which ALL properties and methods are passed down
+// a tree like data structure representative of the given HTML of any web page
+
+
+// 
 
 // constructor function
-function Person() {
-     this.name = 'John',
-     this.age = 23
+function Person(nameParam, ageParam) {
+     this.name = nameParam,
+     this.age = ageParam,
+     this.speak = function() {
+          console.log('hello ' + this.name + ' ' + this.age)
+     }
 }
 
 // create an object
-const person = new Person();
+const person1 = new Person('patrick', 31); // instatiation
+const person2 = new Person('whoeever', 21); 
+// const person2 = new Person('whoeever', 21); 
+// const person2 = new Person('whoeever', 21); 
+// const person2 = new Person('whoeever', 21); 
+// const person2 = new Person('whoeever', 21); 
+
+console.log(person1.speak())
+console.log(person2.speak()) // whoever 21
+
+
+
+// classes in js are syntactical sugar for constructor functions
 
 
 
 
 
+// console.log(this) // global object constructor
+
+// const exampleFx = () => {
+//      console.log(this)  
+//      // this within arrow fx refers to the next outside scope because arrow fx's do NOT have lexical binding of the this keyword
+// }
+
+// function anotherExFx() {
+//      console.log(this) // refer to the function itself
+// }
 
 
 
