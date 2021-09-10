c36d9b71225073435c069a40645468061812336d
Patrick <pjmorgan716@gmail.com>
2021-09-10 11:36:53 -0400

    end & assessment

--- lectures/mongo/day3/assignments/assessment-mongo-v1.js
+++ lectures/mongo/day3/assignments/assessment-mongo-v1.js
@@ -1,7 +1,39 @@
 // assessment #3 - MERN
 
 // You must complete the assessment by the beginning of class Monday 
-// September 13th @ 10:00am est. Handed in via github classroom: 
-// Once finished, please book your code review with me via the following link: 
+// September 13th @ 10:00am est. Handed in via github classroom: https://classroom.github.com/a/pf7INuAG
+// Once finished, please book your code review with me via the following link: https://calendly.com/patrick-morgan-bkny/c136-assessment-2
 
-// Section One: MERN
+// Section One: mern-bug-tracker.js
+
+// Purpose: You have just joined a new high growth startup and have been tasked with your first new feature
+// buildout since being promoted to team lead. Your task is to to build a 'bug tracker' that can more easily 
+// track bugs in your codebase allowing for more efficiency from your team.
+
+// Getting Started:
+// Step-1: Backend Routes & ORM
+// Using Node.js, Mongoose, MongoDB and Postman create all 6 of your CRUD routes
+// You should have two collections: Users & Bugs
+// Screenshot, videocast/screenrecord, or gif these routes working and include them in your PR or assets folder
+
+// Step-3: Consuming Your API Client Side
+// Within a Client folder create a font end application using React.js that allows for:
+// 1) The creation of new users
+// 2) That user should then be able to create adn interact with 'bugs'
+
+// Create a user form to add bugs, it should contain following field:
+//            - Title
+//            - Description
+//            - Time (should auto-fill with system date &time)
+//            - Date (should auto-fill with system date &time)
+//            - Assignee
+// All Data created should be saved in MongoDB. And displayed in an INTERACTIVE table
+// Build a programming logic for SLA that every bug should be resolve in 3 days, after third the day the unresolved bug should be highlighted.
+// Add one more field to show the leftover days to fix the bug (Total time is 3 days from the day bug is assigned.
+
+// Step-4: Styling
+// Make you entire application fully-styled and responsive per the past requirements.
+
+// Outcome:
+// Testing is NOT required for this assessment. You are more than welcome to follow TDD but I will not be helpign or
+// checking any of your testing code. The focus here should be on mongo, mongoose, node, and react.
--- lectures/mongo/day3/notes.js
+++ lectures/mongo/day3/notes.js
@@ -8,6 +8,50 @@
 
 
 
+// users => collection
+// user => resource
+
+
+// to-do's => collection
+// to-do => resource (represented by a singular document)
+
+
+
+
+
+// BASE (non-relational)-basic vs ACID (relational)-rigid
+// basically available 
+// soft-state 
+// eventually consistent
+
+
+
+
+// Atomicity => either the transaction as awhole is excuted or fails (all or nothing)
+// Consistincy => all of your data must confine to all constraints (triggers, cascades)
+// isolation => concurency (controlling things independently)
+// Durability => all changes made to DB are permanent once a singualr transaction is completw
+
+
+// IAM
+// Auth0 (OAuth) => OCTA ()
+
+// SSO (single sign on) 
+// MFA (multi-factor-aithentication) => two-factor, 
+// 
+
+
+
+
+// state management
+// single source of truth located outside of our DF
+
+// it allows for the the passing of data between components
+// that are not within the normal flow of data
+
+// redux is a state management tool made by FB to combat prop drilling by Dan Abramov
+// redux is written in the Context API (providers <-> consumers)
+
 
 
 
