96edee0d881dcbc29609f39fef6b1ffdeb589ba3
Patrick <pjmorgan716@gmail.com>
2021-09-13 11:44:32 -0400

    end

--- lectures/gql/day0/notes.js
+++ lectures/gql/day0/notes.js
@@ -1,31 +1,57 @@
 // notes.js
 
+// today = assessment code reviews & finishing => or assignment gql 1
+// tmrw = assignment gql1 & graph ql practice 1
+// wed = more gql lecture, practice & assignment 2
+// thurs = more gql lecture & assessment 4 capstone
+// at least monday afternoon to fnish the assessment
+// tuesday = code reviews all day for me & intro to one app w/ one app assignment 1
+// wed = one app assignment 2, interview prep, fill in knowledge gaps
+// thurs = interview prep, fill in knowledge gap
+// fri = interview prep, fill in knowledge gap
+
+
+// few ways to persit data
+// js based
+
+// cookies
+// session storage/localStorage
+
+// databases
+
+
+// req.query === POST
+// req.body, req.header
 
 
 
 
+// JWT's
+// header, payload, signature
+
+// const newToken = jwt('header', payload, sh256())
 
 
+// one-way-fx's
 
 
+// deterministic fx's 
+// all fx's in js are deterministic
+// JS has 1st class functions meanign we treat them just like variable:
+
+// 1) pass fx's as arguments ot other fx's
+// 2) fx's can be stored as values 
+// 3) they give us a predictable output given a similar message (input)
+
+// react components => pure functions but they also exhibit charecteristics of 1st class functions
+// it must NOT try to interact or agument its inputs (props) => you can never trust or should never count on props as a means to set state or create new data
+
+const someFx = () => {
+
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// a singular file should export a singular component
 
 
 
